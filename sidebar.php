1e1ee9693256c780ce216a246dd2416098222198
jjclinton <jjclinton15@gmail.com>
2019-01-20 13:50:57 +0100

    first

---
+++ sidebar.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Anne Marijke
+ * Date: 15-1-2019
+ * Time: 21:09
+ */
+?>
+<!-- zijnavigatie -->
+<div class="sidebar">
+	<div class="sidebar-inner">
+		<div class="sidebar-logo">
+			<img src="imgs/final-logo-example.png"/>
+		</div>
+		<nav class="sidenav">
+			<ul class="sidebar-menu">
+				<li><a href="#">Neerslag</a></li>
+				<li><a href="#">Zon</a></li>
+				<li><a href="#">Luchtvochtigheid</a></li>
+			</ul>
+			<button class="logout-button"><a href="?logout=1">Logout</a></button>
+		</nav>
+	</div>
+</div>
+<div class="container">
