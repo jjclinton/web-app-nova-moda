333c8560e2363a14e0894e884401beaa914d749c
Faimire <faimireg@hotmail.com>
2019-01-27 18:59:07 +0100

    cleaned up

--- js/cron.js
+++ js/cron.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     function updater() {
         //table names for calling
         var tables = ["temp-table france", "temp-table esp", "temp-table mex", "temp-table us", "temp-table np", "temp-table sp"];
-        //gets the data from a php file and parses it into a json varibale
+        //gets the data from a php file and parses it into a json variable
         var json_php_data = [];
             $.ajax({
                 url : 'http://localhost:63342/novamoda/encoder.php',
@@ -23,12 +23,16 @@ $(document).ready(function () {
             var json_data = data;
             for(var i = 1; i < amount; i++){
                 var start = 0;
+                //random number for generating random values for the tables
                 var random = Math.floor(Math.random() * 10);
+                //check breakpoints
                 console.log(random);
                 var selected_json = json_data[random];
                 console.log(selected_json);
+                //calculates the windchill
                 var feel = (10 * Math.sqrt(parseFloat(selected_json["WDSP"])) - parseFloat(selected_json["WDSP"]) + 10.5) * (33 - parseFloat(selected_json["TEMP"]));
                 feel = feel.toFixed(2);
+                //loops through the selected keys
                 $.each(fields, function (index, field) {
                     var selected_value = selected_json[field];
                     var x = document.getElementById(tableId).rows[i].cells;
